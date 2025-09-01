9bf32e7b68558635ea4d0fa3f5d9fea24165e9cf
Hell of the Devil <hell_of_the_devil@hotmail.co.uk>
2025-09-02 00:03:41 +0100

    Working package

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='industrious',
+    version='0.0.1',
+    description='A random assortment of utilities based around my personal stack workflow',
+    url='https://github.com/hell-of-the-devil/industrious',
+    author='Hell of the Devil',
+    author_email='me@hell-of-the-devil.me',
+    license='BSD 2-clause',
+    packages=['industrious'],
+    install_requires=[
+
+    ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Operating System :: POSIX :: Linux :: Windows',
+        'Programming Language :: Python :: 3+',
+    ],
+)
