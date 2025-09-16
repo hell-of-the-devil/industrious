47c7a2385354c6b221842a2054b62e79e0d04197
Tommy Haddon <hell_of_the_devil@hotmail.co.uk>
2025-09-17 00:08:24 +0100

    Update __version__.py

--- industrious/__version__.py
+++ industrious/__version__.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __author__ = 'Hell of the Devil'
 __credits__ = 'Myself :) (tbh though, credits are splattered through the code, i will pull them together here at some point)'
